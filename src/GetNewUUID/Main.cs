e4407ee293f1000682d5bc5669d771a3e5fdd750
SeppPenner <HansM2013@gmx.de>
2022-02-15 15:12:57 +0100

    Added nullable checks, added editorconfig, added file scoped namespaces, added global usings.

--- src/GetNewUUID/Main.cs
+++ src/GetNewUUID/Main.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Main.cs" company="Hämmer Electronics">
 //   Copyright (c) All rights reserved.
 // </copyright>
@@ -7,99 +7,99 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace GetNewUUID
-{
-    using System;
-    using System.Windows.Forms;
+namespace GetNewUUID;
 
-    using Languages.Implementation;
-    using Languages.Interfaces;
+/// <summary>
+/// The main form.
+/// </summary>
+public partial class Main : Form
+{
+    /// <summary>
+    /// The language manager.
+    /// </summary>
+    private readonly ILanguageManager languageManager = new LanguageManager();
 
     /// <summary>
-    /// The main form.
+    /// The language.
     /// </summary>
-    public partial class Main : Form
+    private ILanguage? language;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Main"/> class.
+    /// </summary>
+    public Main()
     {
-        /// <summary>
-        /// The language manager.
-        /// </summary>
-        private readonly ILanguageManager languageManager = new LanguageManager();
+        this.InitializeComponent();
+        this.LoadTitleAndDescription();
+        this.InitializeLanguageManager();
+        this.LoadLanguagesToCombo();
+    }
 
-        /// <summary>
-        /// The language.
-        /// </summary>
-        private ILanguage? language;
+    /// <summary>
+    /// Initializes the language manager.
+    /// </summary>
+    private void InitializeLanguageManager()
+    {
+        this.languageManager.SetCurrentLanguage("de-DE");
+        this.languageManager.OnLanguageChanged += this.OnLanguageChanged;
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Main"/> class.
-        /// </summary>
-        public Main()
+    /// <summary>
+    /// Loads all languages to the combo box.
+    /// </summary>
+    private void LoadLanguagesToCombo()
+    {
+        foreach (var lang in this.languageManager.GetLanguages())
         {
-            this.InitializeComponent();
-            this.LoadTitleAndDescription();
-            this.InitializeLanguageManager();
-            this.LoadLanguagesToCombo();
+            this.comboBoxLanguage.Items.Add(lang.Name);
         }
 
-        /// <summary>
-        /// Initializes the language manager.
-        /// </summary>
-        private void InitializeLanguageManager()
+        this.comboBoxLanguage.SelectedIndex = 0;
+    }
+
+    /// <summary>
+    /// Handles the selected index changed event.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The event args.</param>
+    private void ComboBoxLanguageSelectedIndexChanged(object sender, EventArgs e)
+    {
+        var selectedItem = this.comboBoxLanguage.SelectedItem.ToString();
+
+        if (string.IsNullOrWhiteSpace(selectedItem))
         {
-            this.languageManager.SetCurrentLanguage("de-DE");
-            this.languageManager.OnLanguageChanged += this.OnLanguageChanged;
+            return;
         }
 
-        /// <summary>
-        /// Loads all languages to the combo box.
-        /// </summary>
-        private void LoadLanguagesToCombo()
-        {
-            foreach (var lang in this.languageManager.GetLanguages())
-            {
-                this.comboBoxLanguage.Items.Add(lang.Name);
-            }
+        this.languageManager.SetCurrentLanguageFromName(selectedItem);
+    }
 
-            this.comboBoxLanguage.SelectedIndex = 0;
-        }
+    /// <summary>
+    /// Handles the language changed event.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The event args.</param>
+    private void OnLanguageChanged(object sender, EventArgs e)
+    {
+        this.language = this.languageManager.GetCurrentLanguage();
+        this.buttonNewUUID.Text = this.language.GetWord("CreateNewUUID");
+    }
 
-        /// <summary>
-        /// Handles the selected index changed event.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The event args.</param>
-        private void ComboBoxLanguageSelectedIndexChanged(object sender, EventArgs e)
-        {
-            this.languageManager.SetCurrentLanguageFromName(this.comboBoxLanguage.SelectedItem.ToString());
-        }
+    /// <summary>
+    /// Loads the title and description.
+    /// </summary>
+    private void LoadTitleAndDescription()
+    {
+        this.Text = Application.ProductName + @" " + Application.ProductVersion;
+    }
 
-        /// <summary>
-        /// Handles the language changed event.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The event args.</param>
-        private void OnLanguageChanged(object sender, EventArgs e)
-        {
-            this.language = this.languageManager.GetCurrentLanguage();
-            this.buttonNewUUID.Text = this.language.GetWord("CreateNewUUID");
-        }
-
-        /// <summary>
-        /// Loads the title and description.
-        /// </summary>
-        private void LoadTitleAndDescription()
-        {
-            this.Text = Application.ProductName + @" " + Application.ProductVersion;
-        }
-
-        /// <summary>
-        /// Handles the event to generate a new UUID.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The event args.</param>
-        private void NewUuidClick(object sender, EventArgs e)
-        {
-            this.textBoxUUID.Text = Guid.NewGuid().ToString();
-        }
+    /// <summary>
+    /// Handles the event to generate a new UUID.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The event args.</param>
+    private void NewUuidClick(object sender, EventArgs e)
+    {
+        this.textBoxUUID.Text = Guid.NewGuid().ToString();
     }
 }
